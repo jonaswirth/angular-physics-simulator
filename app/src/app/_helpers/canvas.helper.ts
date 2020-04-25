d01431e0c81c7735726813b419c6238789bb7196
Jonas Wirth <jonas-wirth@gmx.ch>
2020-04-25 23:44:31 +0200

    Improve grid

--- app/src/app/_helpers/canvas.helper.ts
+++ app/src/app/_helpers/canvas.helper.ts
@@ -13,6 +13,10 @@ export class CanvasHelper{
     private heightY:number;
 
     private widthPerStep:number;
+    private heightPerStep:number;
+
+    //Display
+    private margin:number = 5;
 
     constructor (width:number, height:number, grid:Grid){
         this.canvasWidth = width;
@@ -28,18 +32,19 @@ export class CanvasHelper{
         console.log("height: " + this.heightY + " width: " + this.widthX);
 
         this.widthPerStep = this.canvasWidth / (this.widthX * this.grid.stepSize);
+        this.heightPerStep = this.canvasHeight / (this.heightY * this.grid.stepSize);
     }
 
     public GetOrigin():Position{
         return {
-            x:5,
-            y:0
+            x:3,
+            y:3
         };
     }
 
     public DrawGrid(ctx:CanvasRenderingContext2D){
         let countLinesX = Math.round(this.canvasWidth/ this.widthPerStep);
-        let countLinesY = Math.round(this.canvasHeight/ (this.heightY * this.grid.stepSize));
+        let countLinesY = Math.round(this.canvasHeight/ this.heightPerStep);
 
         let origin = this.GetOrigin();
 
@@ -47,20 +52,41 @@ export class CanvasHelper{
         console.log(this.widthX * this.grid.stepSize);
         console.log(countLinesX);
 
+        //Draw vertical (x) lines
         for(let i = 0; i <= countLinesX; i++){
             ctx.beginPath();
 
             if(i == origin.x){
                 ctx.strokeStyle = "#000000";
             }
+            else if(i % this.grid.highleightStep == 0){
+                ctx.strokeStyle = "#d1d1d1";
+            }
             else{
                 ctx.strokeStyle = "#e9e9e9";
             }
 
-            ctx.moveTo(i * this.widthPerStep * this.grid.stepSize, 0);
-            console.log("move to: " + i * this.widthPerStep * this.grid.stepSize + ", 0");
-            ctx.lineTo(i * this.widthPerStep * this.grid.stepSize, this.canvasHeight);
-            console.log("line to: " + i * this.widthPerStep * this.grid.stepSize + ", " + this.canvasHeight);
+            ctx.moveTo(i * this.widthPerStep * this.grid.stepSize + this.margin, 0);
+            ctx.lineTo(i * this.widthPerStep * this.grid.stepSize + this.margin, this.canvasHeight);
+            ctx.stroke();
+        }
+
+        //Draw horizontal (y) lines
+        for(let i = 0; i <= countLinesY; i++){
+            ctx.beginPath();
+
+            if(i == origin.y){
+                ctx.strokeStyle = "#000000";
+            }
+            else if(i % this.grid.highleightStep == 0){
+                ctx.strokeStyle = "#d1d1d1";
+            }
+            else{
+                ctx.strokeStyle = "#e9e9e9";
+            }
+
+            ctx.moveTo(0 + this.margin, this.heightY * this.heightPerStep - (i * this.heightPerStep * this.grid.stepSize));
+            ctx.lineTo(this.canvasWidth,  this.heightY * this.heightPerStep - (i * this.heightPerStep * this.grid.stepSize));
             ctx.stroke();
         }
     }
